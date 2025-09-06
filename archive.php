d08c15073ee81778f5a33d7bbb4c62107e10203a
lanbinleo <lanbinwolf@gmail.com>
2025-09-06 23:49:29 +0800

    feat(post-edit): add edit button for logged-in users on post and archive pages

--- archive.php
+++ archive.php
@@ -65,6 +65,15 @@
                                         <?php $this->category(','); ?>
                                     </span>
                                     <?php endif; ?>
+                                    <?php if (false&&$this->user->hasLogin()): ?>
+                                        <a href="/admin/write-post.php?cid=<?php echo $this->cid; ?>" class="post-edit-btn" title="编辑文章">
+                                            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                            </svg>
+                                            <span>&nbsp;编辑文章</span>
+                                        </a>
+                                    <?php endif; ?>
                                 </div>
                             </header>
                             
--- post.php
+++ post.php
@@ -52,7 +52,18 @@
             <?php $this->category(','); ?>
           </span>
           <?php endif; ?>
-          
+          <span class="post-edit-btn">
+          <?php if ($this->user->hasLogin()): ?>
+              <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+              </svg>
+            <a href="/admin/write-post.php?cid=<?php echo $this->cid; ?>"  title="编辑文章">
+              <span>编辑文章</span>
+            </a>
+          <?php endif; ?>
+          </span>
+
           <span class="post-views">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
