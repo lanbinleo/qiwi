168ff7f1b4fa592016debbf766d0244a1ca9fe3e
lanbinleo <lanbinwolf@gmail.com>
2025-11-14 17:04:25 +0800

    feat(travellings): Add option to enable/disable "Travellings" link in the navigation

--- header.php
+++ header.php
@@ -61,6 +61,16 @@
                 <li><a href="<?php $pages->permalink(); ?>"<?php if ($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?>><?php $pages->title(); ?></a></li>
             <?php endwhile; ?>
 
+            <?php if ($this->options->enableTravellings == 1): ?>
+            <li>
+                <a href="https://www.travellings.cn/go.html" target="_blank" rel="noopener noreferrer" title="开往-友链接力">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="14" height="14" fill="currentColor" style="vertical-align: -2px; margin-right: 4px;">
+                        <path d="M0 96C0 43 43 0 96 0L288 0c53 0 96 43 96 96l0 256c0 40.1-24.6 74.5-59.5 88.8l53.9 63.7c8.6 10.1 7.3 25.3-2.8 33.8s-25.3 7.3-33.8-2.8l-74-87.5-151.3 0-74 87.5c-8.6 10.1-23.7 11.4-33.8 2.8s-11.4-23.7-2.8-33.8l53.9-63.7C24.6 426.5 0 392.1 0 352L0 96zm64 32l0 96c0 17.7 14.3 32 32 32l72 0 0-160-72 0c-17.7 0-32 14.3-32 32zM216 256l72 0c17.7 0 32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-72 0 0 160zM96 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm224-32a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/>
+                    </svg>开往
+                </a>
+            </li>
+            <?php endif; ?>
+
             <li><button class="theme-toggle" onclick="toggleTheme()">◐</button></li>
         </ul>
     </div>
